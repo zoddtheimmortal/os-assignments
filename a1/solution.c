afdeca196438474f03650a7a9b71bd5c329ba0b3
Alok Desai <alokdesai2004@gmail.com>
2024-09-14 21:30:27 +0530

    feat: add fn w/o threads

---
+++ a1/solution.c
@@ -0,0 +1,184 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/msg.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+
+#define PERMS 0644
+#define FNAME_SIZE 100
+#define READ_END 0
+#define WRITE_END 1
+#define BUFF_SIZE 200
+#define MSG_LEN 100
+
+int MATRIX_SIZE,MAX_LEN,SHM_KEY,MSG_KEY;
+char infile[FNAME_SIZE],wordfile[FNAME_SIZE];
+
+typedef struct message{
+    long mtype;
+    int key;
+} message;
+
+char* decode_caesar(char* str,int shift){
+    int len=strlen(str);
+    char* res=(char*)malloc((len+1)*sizeof(char));
+
+    for(int i=0;i<len;i++){
+        res[i]=(str[i]-'a'+shift)%26+'a';
+    }
+    res[len]='\0';
+    return res;
+}
+
+int get_wordcount(char* word, char* filename) {
+    int fd[2];
+    
+    if(pipe(fd)==-1){
+        perror("Pipe failed: check get_wordcount fn\n");
+        exit(1);
+    }
+
+    pid_t pid;
+    pid=fork();
+
+    if(pid==-1){
+        perror("Fork failed: check get_wordcount fn\n");
+        exit(1);
+    }
+
+    if (pid==0) {
+        close(fd[READ_END]); 
+        dup2(fd[WRITE_END],STDOUT_FILENO);
+        close(fd[WRITE_END]);
+
+        char command[200];
+        snprintf(command,sizeof(command),"grep -o '\\b%s\\b' %s | wc -l",word,filename);
+        execlp("sh", "sh", "-c", command, NULL);
+
+        perror("Error in execlp: check get_wordcount\n");
+        exit(1);
+    } else {
+        close(fd[WRITE_END]); 
+        wait(NULL); 
+
+        char buffer[BUFF_SIZE];
+        int count=0;
+        if (read(fd[READ_END],buffer,BUFF_SIZE*sizeof(char))!=-1) {
+            printf("Count: %s\n",buffer);
+            count = atoi(buffer);
+        }
+        close(fd[READ_END]);
+        return count;
+    }
+}
+
+void read_keys_from_file(char* filename){
+    FILE* file=fopen(filename,"r");
+    if(file==NULL){
+        perror("No such file exists\n");
+        exit(1);
+    }
+
+    fscanf(file,"%d",&MATRIX_SIZE);
+    fscanf(file,"%d",&MAX_LEN);
+    fscanf(file,"%d",&SHM_KEY);
+    fscanf(file,"%d",&MSG_KEY);
+
+    fclose(file);
+}
+
+int main(int argc,char** argv){
+    int file_idx=atoi(argv[1]);
+
+    sprintf(infile,"input%d.txt",file_idx);
+    sprintf(wordfile,"words%d.txt",file_idx);
+
+    read_keys_from_file(infile); 
+
+    int shmid;
+    char (*shmptr)[MATRIX_SIZE][MAX_LEN];
+
+    if((shmid=shmget(SHM_KEY,sizeof(char[MATRIX_SIZE][MATRIX_SIZE][MAX_LEN]),PERMS))==-1){
+        perror("Error in shmget: check main\n");
+        exit(1);
+    }
+
+    if((shmptr=shmat(shmid,NULL,0))==(void*)-1){
+        perror("Error in shmat: check main\n");
+        exit(1);
+    }
+
+    int shift=0;
+    int msqid;
+
+    if((msqid=msgget(MSG_KEY,PERMS|IPC_CREAT))==-1){
+        perror("Error in msgget: check main\n");
+        exit(1);
+    }
+
+    for(int c=0;c<MATRIX_SIZE;c++){
+        int i=0,j=c;
+        int cnt=0;
+
+        while(i<MATRIX_SIZE&&j>=0){
+            char* word=decode_caesar(shmptr[i][j],shift);
+            printf("Word: %s\n",word);
+            cnt+=get_wordcount(word,wordfile);
+            i++;
+            j--;
+        }
+
+        message msg;
+        msg.mtype=1;
+        msg.key=cnt;
+
+        if(msgsnd(msqid,&msg,sizeof(msg)-sizeof(msg.mtype),0)==-1){
+            perror("Error in msgsnd: check main\n");
+            exit(1);
+        }
+
+        if(msgrcv(msqid,&msg,sizeof(msg)-sizeof(msg.mtype),2,0)==-1){
+            perror("Error in msgrcv: check main\n");
+            exit(1);
+        }
+
+        shift=msg.key;
+     }
+
+    for(int r=1;r<MATRIX_SIZE;r++){
+        int cnt=0;
+        int i=r,j=MATRIX_SIZE-1;
+
+        while(i<MATRIX_SIZE&&j>=0){
+            char* word=decode_caesar(shmptr[i][j],shift);
+            cnt+=get_wordcount(word,wordfile);
+            i++;
+            j--;
+        }
+
+        message msg;
+        msg.mtype=1;
+        msg.key=cnt;
+
+        if(msgsnd(msqid,&msg,sizeof(msg)-sizeof(msg.mtype),0)==-1){
+            perror("Error in msgsnd: check main\n");
+            exit(1);
+        }
+
+        if(msgrcv(msqid,&msg,sizeof(msg)-sizeof(msg.key),2,0)==-1){
+            perror("Error in msgrcv: check main\n");
+            exit(1);
+        }
+
+        shift=msg.key;
+    }
+
+    if((shmdt(shmptr))==-1){
+        perror("Error in shmdt: check main\n");
+        exit(1);
+    }
+}
